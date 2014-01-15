8128e05118b87936b27306cb042ecd4114e7c0f5
bmackin <mackin.beth@gmail.com>
2014-01-14 22:00:29 -0500

    commit 6-2

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,11 @@
-#commit 6-1
-
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-	grocery_list.collect!{|item| "* " + item }
-	puts grocery_list
+
+#commit 6-2 - grocery list add rice
+grocery_list << "rice"
+
+#			- method to output list
+def output_arr(arr)
+	arr.each{|item| puts "* " + item }
+end
+
+output_arr(grocery_list)
