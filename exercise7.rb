02de7892544ff3b3f5b8dc4bd23814f3b5506123
bmackin <mackin.beth@gmail.com>
2014-01-15 14:19:20 -0500

    commit 7-1.rb

---
+++ exercise7.rb
@@ -0,0 +1,58 @@
+# commit 7-1 Create and dislplay hash
+students = {
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22
+}
+
+def output_hash(hash)
+	hash.each{ |key, value| puts "#{key} #{value}"}
+end
+
+output_hash(students)
+
+
+# commit 7-2 Add to hash
+students.merge!(:cohort4 => 43)
+
+output_hash(students)
+
+# commit 7-3 Output cohort names with keys method
+puts students.keys
+
+
+# commit 7-4 Increase number of students 5%
+def student_increase(hash)
+	hash.each{ |key, value| hash[key] = (value * 1.05).to_i }
+end
+
+student_increase(students)
+output_hash(students)
+
+# commit 7-5 Delete 2nd cohort, redisplay
+students.delete(:cohort2)
+
+output_hash(students)
+
+# commit 7-6 BONUS: Calculate the total amount of students in all cohorts by using each and incrementing a variable. Output the result.
+
+num_of_students = [students.values]
+num_of_students.each.inject{ |sum, v| sum + v}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
